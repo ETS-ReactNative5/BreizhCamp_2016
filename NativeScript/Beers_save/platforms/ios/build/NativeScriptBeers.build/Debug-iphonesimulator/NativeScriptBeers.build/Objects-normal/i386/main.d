522fa108eb188338e969b45d6d1a117090538cc9
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2022-01-11 15:13:21 +0100

    fix: remove node_moddules useless

--- NativeScript/Beers_save/platforms/ios/build/NativeScriptBeers.build/Debug-iphonesimulator/NativeScriptBeers.build/Objects-normal/i386/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/stephanie/Development/NativeScript/NativeScript-Beers/platforms/ios/internal/main.m \
-  /Users/stephanie/Development/NativeScript/NativeScript-Beers/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/stephanie/Development/NativeScript/NativeScript-Beers/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/stephanie/Development/NativeScript/NativeScript-Beers/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/stephanie/Development/NativeScript/NativeScript-Beers/platforms/ios/internal/TNSDebugging.h
--- NativeScript/Beers_save/platforms/ios/build/emulator/NativeScriptBeers.app/app/app.js
+++
@@ -1,9 +0,0 @@
-var application = require("application");
-application.mainModule = "main-page";
-application.cssFile = "./app.css";
-
-application.on(application.launchEvent, function (args) {
-   var mainPage = require("./main-page");
-   mainPage.beers();
-});
-application.start();
